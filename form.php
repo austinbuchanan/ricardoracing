66cf9ce705d04f9b9d2a377d08645fc41eecfaba
David Pridemore <me@davidpridemore.com>
2016-10-14 12:17:35 -0500

    Server Side Events Messages

--- form.php
+++ form.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-$_SESSION['username'] = $_REQUEST['username'];
-$_SESSION['password'] = $_REQUEST['password'];
-?>
+$_SESSION['username'] = $_POST['username'];
+$_SESSION['password'] = $_POST['password'];
 
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -13,238 +13,98 @@ $_SESSION['password'] = $_REQUEST['password'];
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="styles.css">
-		<script src="jquery-3.1.1.min.js"></script>
-		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-		<script type = "text/javascript">
-		
-			$( document ).ready(function() {
-				
-				alert( "ready!" );
-				
-				/*$( function() {
-				var progressTimer,
-				  progressbar = $( "#progressbar" ),
-				  progressLabel = $( ".progress-label" ),
-				  dialogButtons = [{
-					text: "Cancel Download",
-					click: closeDownload
-				  }],
-				  dialog = $( "#dialog" ).dialog({
-					autoOpen: false,
-					closeOnEscape: false,
-					resizable: false,
-					buttons: dialogButtons,
-					open: function() {
-					  progressTimer = setTimeout( progress, 2000 );
-					},
-					beforeClose: function() {
-					  downloadButton.button( "option", {
-						disabled: false,
-						label: "Start Download"
-					  });
-					}
-				  }),
-				  downloadButton = $( "#submit-horses" )
-					.button()
-					.on( "click", function() {
-					  $( this ).button( "option", {
-						disabled: true,
-						label: "Downloading..."
-					  });
-					  dialog.dialog( "open" );
-					});
-			 
-				progressbar.progressbar({
-				  value: false,
-				  change: function() {
-					progressLabel.text( "Current Progress: " + progressbar.progressbar( "value" ) + "%" );
-				  },
-				  complete: function() {
-					progressLabel.text( "Complete!" );
-					dialog.dialog( "option", "buttons", [{
-					  text: "Close",
-					  click: closeDownload
-					}]);
-					$(".ui-dialog button").last().trigger( "focus" );
-				  }
-				});
-			 
-				function progress() {
-				  var val = progressbar.progressbar( "value" ) || 0;
-			 
-				  progressbar.progressbar( "value", val + Math.floor( Math.random() * 3 ) );
-			 
-				  if ( val <= 99 ) {
-					progressTimer = setTimeout( progress, 50 );
-				  }
-				}
-			 
-				function closeDownload() {
-				  clearTimeout( progressTimer );
-				  dialog
-					.dialog( "option", "buttons", dialogButtons )
-					.dialog( "close" );
-				  progressbar.progressbar( "value", false );
-				  progressLabel
-					.text( "Starting download..." );
-				  downloadButton.trigger( "focus" );
-				}
-			  });*/
-				
-				$("#horses-form").submit(function(event) {
-					
-				/*var callAjax = function(){
-					$.ajax({
-						method:'get',
-						data:
-						dataType: "json",
-						url:'scraper.php',
-						success:function(data){
-							response = jQuery.parseJSON(data);
-							document.getElementById('progress').innerHTML += response;
-						}
-					});
-				}
-				
-				setInterval(callAjax,3000);*/
-				
-				var previous = "";
-
-				setInterval(function() {
-					var ajax = new XMLHttpRequest();
-					ajax.onreadystatechange = function() {
-						if (ajax.readyState == 4) {
-							if (ajax.responseText != previous) {
-								document.getElementById('progress').innerHTML = previous + "<br>";
-								previous = ajax.responseText;
-							}
-						}
-					};
-					ajax.open("POST", "progress.txt", true); //Use POST to avoid caching
-					ajax.send();
-				}, 1);
-
-				event.preventDefault();
-
-				var $form = $( this ),
-				url = $form.attr( 'action' );
-
-				var posting = $.post( url, 
-				{ 
-					horsenames: $('#horsenames').val(), 
-					filter: $('#filter').val(),
-					gens: $('#gens').val(),
-					crosses: $('#crosses').val(),
-					influence: $('#influence').val(),
-				});
-
-					posting.done(function( data ) 
-					{
-						document.getElementById('download').click();
-						alert('success');
-					});
-				});
-			});
-		</script>
+		<script src="jquery.js"></script>
     </head>
     <body>
-		
-		<div id="horse_form">
-			<form id="horses-form" action="scraper.php" title="" method="post">
-				<table style="padding-top:60px;">
-					<tr>
-						<td>
-							<label><strong>Enter a horse's name on each line:</strong><label>
-							<br />
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<textarea rows="30" id="horsenames"></textarea>
-							<br />
-							<label><strong>Enter a filter:</strong></label>
-							<select id="filter">
-								<option value="Cross+Dups">Cross Dups</option>
-								<option value="Dups+Only">Dups Only</option>
-								<option value="Males+Only">Males Only</option>
-								<option value="Females+Only">Females Only</option>
-								<option value="All+Horses">All Horses</option>
-							</select>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label><strong>Enter the number of generations:</strong></label>
-							<select id="gens">
-								<option value="4">4</option>
-								<option value="5">5</option>
-								<option value="6">6</option>
-								<option value="7">7</option>
-								<option value="8">8</option>
-								<option value="9">9</option>
-								<option value="10">10</option>
-								<option value="11">11</option>
-								<option value="12">12</option>
-								<option value="13">13</option>
-								<option value="14">14</option>
-								<option value="15">15</option>
-								<option value="16">16</option>
-								<option value="17">17</option>
-								<option value="18">18</option>
-								<option value="19">19</option>
-								<option value="20">20</option>
-							</select>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label><strong>Enter the number of crosses:</strong></label>
-							<select id="crosses">
-								<option value="2">2</option>
-								<option value="3">3</option>
-								<option value="4">4</option>
-								<option value="5">5</option>
-								<option value="6">6</option>
-								<option value="7">7</option>
-								<option value="8">8</option>
-								<option value="9">9</option>
-								<option value="10">10</option>
-							</select>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label><strong>Enter the influence:</strong></label>
-							<select id="influence">
-								<option value="All">All</option>
-								<option value="> 2x3">> 2x3</option>
-								<option value="> 3x3">> 3x3</option>
-								<option value="> 3x4">> 3x4</option>
-								<option value="> 4x4">> 4x4</option>
-								<option value="> 4x5">> 4x5</option>
-								<option value="> 5x5">> 5x5</option>
-								<option value="> 5x6">> 5x6</option>
-								<option value="All2">All</option>
-							</select>
-							
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<input id="submit-horses" type="submit" />
-						</td>
-					</tr>
-				</table>
-			</form>
-		</div>
-		<!--<div id="dialog" title="File Download">
-		<div class="progress-label">Starting download...</div>
-		<div id="progressbar"></div>
-		</div>-->
-		<div id="progress"></div>
-		<div id="output">
-			<a href="horses.csv" download id="download" hidden></a>
-		</div>
+	<div id="horse_form">
+		<form name="horses-form" action="scraper.php" method="post">
+			<table style="padding-top:60px;"
+				<tr>
+					<td>
+						<label><strong>Enter a horse's name on each line:</strong><label>
+						<br />
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<textarea rows="30" name="horsenames"></textarea>
+						<br />
+						<label><strong>Enter a filter:</strong></label>
+						<select name="filter">
+						    <option value="Cross+Dups">Cross Dups</option>
+							<option value="Dups+Only">Dups Only</option>
+							<option value="Males+Only">Males Only</option>
+							<option value="Females+Only">Females Only</option>
+							<option value="All+Horses">All Horses</option>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label><strong>Enter the number of generations:</strong></label>
+						<select name="gens">
+							<option value="4">4</option>
+							<option value="5">5</option>
+							<option value="6">6</option>
+							<option value="7">7</option>
+							<option value="8">8</option>
+							<option value="9">9</option>
+							<option value="10">10</option>
+							<option value="11">11</option>
+							<option value="12">12</option>
+							<option value="13">13</option>
+							<option value="14">14</option>
+							<option value="15">15</option>
+							<option value="16">16</option>
+							<option value="17">17</option>
+							<option value="18">18</option>
+							<option value="19">19</option>
+							<option value="20">20</option>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label><strong>Enter the number of crosses:</strong></label>
+						<select name="crosses">
+							<option value="2">2</option>
+							<option value="3">3</option>
+							<option value="4">4</option>
+							<option value="5">5</option>
+							<option value="6">6</option>
+							<option value="7">7</option>
+							<option value="8">8</option>
+							<option value="9">9</option>
+							<option value="10">10</option>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label><strong>Enter the influence:</strong></label>
+						<select name="influence">
+							<option value="All">All</option>
+							<option value="> 2x3">> 2x3</option>
+							<option value="> 3x3">> 3x3</option>
+							<option value="> 3x4">> 3x4</option>
+							<option value="> 4x4">> 4x4</option>
+							<option value="> 4x5">> 4x5</option>
+							<option value="> 5x5">> 5x5</option>
+							<option value="> 5x6">> 5x6</option>
+							<option value="All2">All</option>
+						</select>
+						
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input id="submit-horses" type="submit" />
+					</td>
+				</tr>
+			</table>
+		</form>
+		<!--<script src="jquery.js"></script>
+        <div id="processor"></div>-->
         
     </body>
 </html>
--- scraper.php
+++ scraper.php
@@ -2,43 +2,44 @@
 session_start();
 /*
 	Licensed under The MIT License
-
+	
 	Copyright (c) 2016 David Pridemore, Austin Buchanan
-	* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the
-	* Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+	* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the 
+	* Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
 	* and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
+	
 	* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-	* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+	* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
 	* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
+	
 	* Redistributions of files must retain the above copyright notice.
 	*
 	* @author David Pridemore <me@davidpridemore.com>
 	* @author Austin Buchanan <abuchanan5@my.apsu.edu>
 */
-
+	//headers for .csv output
+	
+	
 	//open .csv file for writing and create headings for each horse's data
-	$output = fopen('horses.csv', 'w');
-	$progress = fopen('progress.txt', 'w');
+    $output = fopen('php://output', 'w');
 	$heading_array = Array('Horse', 'Relative', 'Inbreeding Stats', 'Crosses', 'Lines', 'Blood%', 'Influence', 'AGR');
-
+	
     //include the amazing simple dom parser that allows us to search through the html of the returned data
 	include('simple_html_dom.php');
-
+	
 	//turn off notices related to dom conversion errors
 	libxml_use_internal_errors(true);
-
+	
 	//take in values from index.htm for each filter and horse name list
-	$horsenames = $_REQUEST['horsenames'];
-	$filter = $_REQUEST['filter'];
-	$crosses = $_REQUEST['crosses'];
-	$gens = $_REQUEST['gens'];
-	$influence = $_REQUEST['influence'];
+	$horsenames = $_POST['horsenames'];
+	$filter = $_POST['filter'];
+	$crosses = $_POST['crosses'];
+	$gens = $_POST['gens'];
+	$influence = $_POST['influence'];
 
 	$inf_num = 0;
-	//influence is weird. the filter menu on allbreedpedigree displays in NumxNum format but requires a decimal in the url
+	//influence is weird. the filter menu on allbreedpedigree displays in NumxNum format but requires a decimal in the url 
 	switch($influence)
 	{
 		case "All":
@@ -68,17 +69,17 @@ session_start();
 			$inf_num = 0.78125;
 			break;
 	}
-
+	
 	//put each horse's name in an array based on a single horse's name entered on each line
 	$horse_name_array = (preg_split('/[\n]+/', $horsenames));
-
-	/* declare urls, checkbreed and display_names arrays. urls will contain URLs to send to allbreedpedigree.com, display
-	names is for .csv formatting, and checkbreed will contain horse names to check <td>s for "Thoroughbred" or "Quarter
+	
+	/* declare urls, checkbreed and display_names arrays. urls will contain URLs to send to allbreedpedigree.com, display 
+	names is for .csv formatting, and checkbreed will contain horse names to check <td>s for "Thoroughbred" or "Quarter 
 	Horse" */
 	$urls = Array();
 	$checkbreed = Array();
 	$display_names = Array();
-
+	
 	//loop through each horse's name and remove annoying whitespace and plus signs (+) from the horses' names for the URL
 	for ($i = 0; $i < count($horse_name_array); $i++)
 	{
@@ -86,59 +87,59 @@ session_start();
 		$horse_name_array[$i] = str_replace(' ', '+', $horse_name_array[$i]);
 		$horse_name_array[$i] = trim(preg_replace('/\s+/', '', $horse_name_array[$i]));
 	}
-
-
+	
+	
 	//make display names in .csv uppercase based on legacy app and client preference
 	$display_names = array_map('strtoupper', $display_names);
-
+	
 	//replace excess whitespace for .csv display purposes
     for($i = 0; $i < count($display_names); $i++)
     {
         $display_names[$i] = trim(preg_replace('/\s+/', ' ', $display_names[$i]));
     }
-
+    
     for ($j = 0; $j < count($horse_name_array); $j++)
-	{
+	{	
 		array_push($checkbreed, "http://www.allbreedpedigree.com/".$horse_name_array[$j]);
 	}
-
-
+    
+    
     ob_start();
 
     $page_data = '';
-
-    function curl($url)
+    
+    function curl($url) 
     {
         // assigning curl options to array
         $options = Array(
-            CURLOPT_RETURNTRANSFER => TRUE,
-            CURLOPT_FOLLOWLOCATION => TRUE,
-            CURLOPT_AUTOREFERER => TRUE,
-            CURLOPT_CONNECTTIMEOUT => 1200,
-            CURLOPT_TIMEOUT => 1200,
-            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_RETURNTRANSFER => TRUE,  
+            CURLOPT_FOLLOWLOCATION => TRUE,  
+            CURLOPT_AUTOREFERER => TRUE, 
+            CURLOPT_CONNECTTIMEOUT => 1200,   
+            CURLOPT_TIMEOUT => 1200,  
+            CURLOPT_MAXREDIRS => 10, 
             CURLOPT_USERAGENT => "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1a2pre) Gecko/2008073000 Shredder/3.0a2pre ThunderBrowse/3.2.1.8",  //setting the useragent
             CURLOPT_URL => $url, //setting curl's url option with the $url variable passed into the function
         );
-
-        $ch = curl_init();
-        curl_setopt_array($ch, $options);
-        $data = curl_exec($ch);
-        curl_close($ch);
-        return $data;
+         
+        $ch = curl_init();   
+        curl_setopt_array($ch, $options);   
+        $data = curl_exec($ch); 
+        curl_close($ch); 
+        return $data; 
     }
 
     foreach ($checkbreed as $cb)
     {
         $page_data = curl($cb);
         echo $page_data;
-	}
+    }
 	$cbpage = ob_get_clean();
-
+	
 	$cbpage = html_entity_decode($cbpage); //get rid of entities
-
+	
 	$first_column = array();
-	$cbhtml = str_get_html($cbpage);
+	$cbhtml = str_get_html($cbpage); 
     $breed = Array();
     foreach($cbhtml->find('title') as $title)
     {
@@ -174,7 +175,7 @@ session_start();
                 //echo $search_html;
                 foreach($search_html->find('td') as $col)
                 {
-
+                
                     if ($col->plaintext == $display_names[$i])
                     {
                         $next_col = $col->next_sibling();
@@ -195,7 +196,7 @@ session_start();
                 }
             }
         }
-        else if(strpos($breed[$i], "Quarter Horse") === FALSE || strpos($breed[$i], "Appendix") !== FALSE)
+        else if(strpos($breed[$i], "Quarter Horse") === FALSE)
         {
             $curl_url = "http://www.allbreedpedigree.com/index.php?query_type=check&search_bar=linebreeding&hypo_sire=&hypo_dam=&what=done&sort=inf&border=0&h=".$horse_name_array[$i]."&g=".$gens."&crosses=".$crosses."&inf=".$inf_num."&all=".$filter."&sort=inf&t=&username=".$_SESSION['username']."&password=".$_SESSION['password'];
             //echo $curl_url."<br>";
@@ -205,7 +206,7 @@ session_start();
             $search_page = ob_get_clean();
             $search_html = str_get_html($search_page);
             $legend = $search_html->find('legend');
-            if (!$legend)
+            if (!$legend && strpos($breed[$i], "Quarter Horse" !== FALSE))
             {
                 array_push($urls, $curl_url);
                 array_push($first_column, $display_names[$i]);
@@ -217,7 +218,7 @@ session_start();
                     if ($col->plaintext == $display_names[$i])
                     {
                         $next_col = $col->next_sibling();
-                        if ($next_col->plaintext == "QUARTER HORSE" || $next_col->plaintext == "APPENDIX")
+                        if ($next_col->plaintext == "QUARTER HORSE")
                         {
                             foreach($col->find('a') as $link)
                             {
@@ -234,54 +235,49 @@ session_start();
             }
         }
     }
-
+    
 	ob_start();
-
-
-	//go curling
-	$incrementer = 0;
 	
-	foreach ($urls as $url)
-	{
-		$urlcount = count($urls);
-		$incrementer++;
+	
+	//go curling
+	foreach ($urls as $url) 
+	{ 
 		$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1200);
 		curl_setopt($ch, CURLOPT_TIMEOUT, 1200);
-
-		curl_exec($ch);
+	
+		curl_exec($ch); 
 		curl_close($ch);
-		fwrite($progress, "Horse ". $incrementer ." of ".$urlcount." processed \n");
 	}
-
+	
 	//output returned curl data as $page
 	$page = ob_get_clean();
-
+	
 	//get rid of all the &lt; and &gt; and other entities in the data
 	$page = html_entity_decode($page);
-
+	
 	//parse the string of data in $page as html in $html
-	$html = str_get_html($page);
-
-
+	$html = str_get_html($page); 
+	
+	
 	//start output buffering again
 	ob_start();
-
+	
 	//declare full_data array
     $full_data = array();
-
+	
 	//simple dom parser find for all images and get rid of them with empty strings
 	foreach($html->find('img') as $images)
 	{
 		$images->outertext = '';
 	}
-
+	
 	//get rid of header class <td> elements
 	foreach($html->find('.header') as $headers)
 	{
 		$headers->outertext = '';
 	}
-
+	
 	//find <td> elements and get rid of all their attributes
 	foreach($html->find('td') as $cells)
 	{
@@ -290,8 +286,8 @@ session_start();
 		$cells->align = null;
 		$cells->colspan = null;
 	}
-
-
+	
+	
 	//find all the data between the <center> elements without a class
 	foreach($html->find('center[!class]') as $horse)
 	{
@@ -301,40 +297,40 @@ session_start();
 		//parse $horse as HTML again
 		$horse = str_get_html($horse);
 		//find all the empty <b><td><tr> and get rid of them
-		foreach($horse->find('b, td, tr') as $element)
+		foreach($horse->find('b, td, tr') as $element) 
 		{
-			if(trim($element->innertext) == '')
+			if(trim($element->innertext) == '') 
 			{
 				$element->outertext = '';
-			}
+			}			
 		}
-
+		
 		//declare rowData array
 		$rowData = array();
-
+		
 		//find all the <tr> and <b> elements to find the rows
-		foreach($horse->find('tr, b') as $row)
+		foreach($horse->find('tr, b') as $row)		
 		{
 			//declare horse_data array
 			$horse_data = array();
-
+			
 			//store <td> element values in plaintext in $horse data
-			foreach($row->find('td') as $cell)
+			foreach($row->find('td') as $cell) 
 			{
 				$horse_data[] = $cell->plaintext;
 			}
-
+			
 			//put the <td> stuff from $horse_data[] into "rows" in $rowData[]
 			$rowData[] = $horse_data;
 		}
-
+		  
 		//put the "rows" into $full_data
         $full_data[] = $rowData;
-
-	}
+		
+	}		
 	//declare raw_data array
     $raw_data = array();
-
+	
 	//loop through $full_data and return the big list of unchunked horse data
     foreach ($full_data as $fd)
     {
@@ -346,7 +342,7 @@ session_start();
     {
         $chunked_data[] = array_chunk($rd, 7);
     }
-
+	
     //adds display names to the first column of each row for the corresponding horse
 
 	for ($i = 0; $i < count($chunked_data); $i++)
@@ -361,16 +357,16 @@ session_start();
 	foreach ($chunked_data as $cd)
     {
 		//remove the inbreeding coefficient number from the bottom of the results and place as the top per client preference
-
+		
 		$coef = array_pop($cd);
 		$coef = implode($coef);
 		$coef = preg_replace("/[^\d,.]/", "", $coef);$coef = $coef."%";
-
+        
         $sort_array = array($first_column[$i], substr($coef, 1), "***");
         fputcsv($output, $sort_array);
 		//output headings after coefficient number
 		fputcsv($output, $heading_array);
-
+		
 		//remove numbers from horse names for duplicate horses.
         foreach ($cd as $second)
         {
@@ -380,15 +376,17 @@ session_start();
 		fwrite($output, "\n");
 		$i++;
     }
-
+	
 	//scraped tables contains all the formatted data for .csv
 	$scraped_tables = ob_get_clean();
-
+	
+	header('Content-Type: text/csv; charset=utf-8');
+	header('Content-Disposition: attachment; filename=horses.csv');
 	//output the data for .csv
-	fwrite($output, $scraped_tables);
+	echo $scraped_tables;
 	//print_r($chunked_data);
-	ob_end_clean();
-	unlink('progress.txt');
+	
+	
 	exit();
-
+	
 ?>
